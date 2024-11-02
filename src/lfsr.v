fbf684436828655436feb686ecef698ddc361e20
Kevin W. Rudd <ke6fzi@gmail.com>
2024-11-02 11:01:20 -0400

    test with length 32->16

--- src/lfsr.v
+++ src/lfsr.v
@@ -37,48 +37,32 @@
 
 module generate_mask_fibonacci_2_taps
 (
-    input  wire [4:0]    lfsr_length,
+    input  wire [3:0]    lfsr_length,
 
-    output reg  [31:0]   mask_value,
+    output reg  [15:0]   mask_value,
     output reg           mask_valid
 );
 
     always @(*)
     begin
         case (lfsr_length)
-               5'd00 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd01 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd02 : begin mask_value   = 32'b00000000000000000000000000000011; mask_valid  = 1; end
-               5'd03 : begin mask_value   = 32'b00000000000000000000000000000110; mask_valid  = 1; end
-               5'd04 : begin mask_value   = 32'b00000000000000000000000000001100; mask_valid  = 1; end
-               5'd05 : begin mask_value   = 32'b00000000000000000000000000010100; mask_valid  = 1; end
-               5'd06 : begin mask_value   = 32'b00000000000000000000000000110000; mask_valid  = 1; end
-               5'd07 : begin mask_value   = 32'b00000000000000000000000001100000; mask_valid  = 1; end
-               5'd08 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd09 : begin mask_value   = 32'b00000000000000000000000100010000; mask_valid  = 1; end
-               5'd10 : begin mask_value   = 32'b00000000000000000000001001000000; mask_valid  = 1; end
-               5'd11 : begin mask_value   = 32'b00000000000000000000010100000000; mask_valid  = 1; end
-               5'd12 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd13 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd14 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd15 : begin mask_value   = 32'b00000000000000000110000000000000; mask_valid  = 1; end
-               5'd16 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd17 : begin mask_value   = 32'b00000000000000010010000000000000; mask_valid  = 1; end
-               5'd18 : begin mask_value   = 32'b00000000000000100000010000000000; mask_valid  = 1; end
-               5'd19 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd20 : begin mask_value   = 32'b00000000000010010000000000000000; mask_valid  = 1; end
-               5'd21 : begin mask_value   = 32'b00000000000101000000000000000000; mask_valid  = 1; end
-               5'd22 : begin mask_value   = 32'b00000000001100000000000000000000; mask_valid  = 1; end
-               5'd23 : begin mask_value   = 32'b00000000010000100000000000000000; mask_valid  = 1; end
-               5'd24 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd25 : begin mask_value   = 32'b00000001001000000000000000000000; mask_valid  = 1; end
-               5'd26 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd27 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd28 : begin mask_value   = 32'b00001001000000000000000000000000; mask_valid  = 1; end
-               5'd29 : begin mask_value   = 32'b00010100000000000000000000000000; mask_valid  = 1; end
-               5'd30 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd31 : begin mask_value   = 32'b01001000000000000000000000000000; mask_valid  = 1; end
-             default : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
+               4'd00 : begin mask_value   = 16'b0000000000000000; mask_valid  = 0; end
+               4'd01 : begin mask_value   = 16'b0000000000000000; mask_valid  = 0; end
+               4'd02 : begin mask_value   = 16'b0000000000000011; mask_valid  = 1; end
+               4'd03 : begin mask_value   = 16'b0000000000000110; mask_valid  = 1; end
+               4'd04 : begin mask_value   = 16'b0000000000001100; mask_valid  = 1; end
+               4'd05 : begin mask_value   = 16'b0000000000010100; mask_valid  = 1; end
+               4'd06 : begin mask_value   = 16'b0000000000110000; mask_valid  = 1; end
+               4'd07 : begin mask_value   = 16'b0000000001100000; mask_valid  = 1; end
+               4'd08 : begin mask_value   = 16'b0000000000000000; mask_valid  = 0; end
+               4'd09 : begin mask_value   = 16'b0000000100010000; mask_valid  = 1; end
+               4'd10 : begin mask_value   = 16'b0000001001000000; mask_valid  = 1; end
+               4'd11 : begin mask_value   = 16'b0000010100000000; mask_valid  = 1; end
+               4'd12 : begin mask_value   = 16'b0000000000000000; mask_valid  = 0; end
+               4'd13 : begin mask_value   = 16'b0000000000000000; mask_valid  = 0; end
+               4'd14 : begin mask_value   = 16'b0000000000000000; mask_valid  = 0; end
+               4'd15 : begin mask_value   = 16'b0110000000000000; mask_valid  = 1; end
+             default : begin mask_value   = 16'b0000000000000000; mask_valid  = 0; end
         endcase
     end // always
 
@@ -88,48 +72,32 @@ endmodule // generate_mask_fibonacci_
 
 module generate_mask_fibonacci_4_taps
 (
-    input  wire [4:0]    lfsr_length,
+    input  wire [3:0]    lfsr_length,
 
-    output reg  [31:0]   mask_value,
+    output reg  [15:0]   mask_value,
     output reg           mask_valid
 );
 
     always @(*)
     begin
         case (lfsr_length)
-               5'd00 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd01 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd02 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd03 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd04 : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
-               5'd05 : begin mask_value   = 32'b00000000000000000000000000011110; mask_valid  = 1; end
-               5'd06 : begin mask_value   = 32'b00000000000000000000000000110110; mask_valid  = 1; end
-               5'd07 : begin mask_value   = 32'b00000000000000000000000001111000; mask_valid  = 1; end
-               5'd08 : begin mask_value   = 32'b00000000000000000000000010111000; mask_valid  = 1; end
-               5'd09 : begin mask_value   = 32'b00000000000000000000000110110000; mask_valid  = 1; end
-               5'd10 : begin mask_value   = 32'b00000000000000000000001101100000; mask_valid  = 1; end
-               5'd11 : begin mask_value   = 32'b00000000000000000000011101000000; mask_valid  = 1; end
-               5'd12 : begin mask_value   = 32'b00000000000000000000110010100000; mask_valid  = 1; end
-               5'd13 : begin mask_value   = 32'b00000000000000000001101100000000; mask_valid  = 1; end
-               5'd14 : begin mask_value   = 32'b00000000000000000011010100000000; mask_valid  = 1; end
-               5'd15 : begin mask_value   = 32'b00000000000000000111010000000000; mask_valid  = 1; end
-               5'd16 : begin mask_value   = 32'b00000000000000001011010000000000; mask_valid  = 1; end
-               5'd17 : begin mask_value   = 32'b00000000000000011110000000000000; mask_valid  = 1; end
-               5'd18 : begin mask_value   = 32'b00000000000000111001000000000000; mask_valid  = 1; end
-               5'd19 : begin mask_value   = 32'b00000000000001110010000000000000; mask_valid  = 1; end
-               5'd20 : begin mask_value   = 32'b00000000000011001010000000000000; mask_valid  = 1; end
-               5'd21 : begin mask_value   = 32'b00000000000111001000000000000000; mask_valid  = 1; end
-               5'd22 : begin mask_value   = 32'b00000000001001110000000000000000; mask_valid  = 1; end
-               5'd23 : begin mask_value   = 32'b00000000011010100000000000000000; mask_valid  = 1; end
-               5'd24 : begin mask_value   = 32'b00000000110110000000000000000000; mask_valid  = 1; end
-               5'd25 : begin mask_value   = 32'b00000001111000000000000000000000; mask_valid  = 1; end
-               5'd26 : begin mask_value   = 32'b00000011100010000000000000000000; mask_valid  = 1; end
-               5'd27 : begin mask_value   = 32'b00000111001000000000000000000000; mask_valid  = 1; end
-               5'd28 : begin mask_value   = 32'b00001100101000000000000000000000; mask_valid  = 1; end
-               5'd29 : begin mask_value   = 32'b00011101000000000000000000000000; mask_valid  = 1; end
-               5'd30 : begin mask_value   = 32'b00110010100000000000000000000000; mask_valid  = 1; end
-               5'd31 : begin mask_value   = 32'b01111000000000000000000000000000; mask_valid  = 1; end
-             default : begin mask_value   = 32'b00000000000000000000000000000000; mask_valid  = 0; end
+               4'd00 : begin mask_value   = 16'b0000000000000000; mask_valid  = 0; end
+               4'd01 : begin mask_value   = 16'b0000000000000000; mask_valid  = 0; end
+               4'd02 : begin mask_value   = 16'b0000000000000000; mask_valid  = 0; end
+               4'd03 : begin mask_value   = 16'b0000000000000000; mask_valid  = 0; end
+               4'd04 : begin mask_value   = 16'b0000000000000000; mask_valid  = 0; end
+               4'd05 : begin mask_value   = 16'b0000000000011110; mask_valid  = 1; end
+               4'd06 : begin mask_value   = 16'b0000000000110110; mask_valid  = 1; end
+               4'd07 : begin mask_value   = 16'b0000000001111000; mask_valid  = 1; end
+               4'd08 : begin mask_value   = 16'b0000000010111000; mask_valid  = 1; end
+               4'd09 : begin mask_value   = 16'b0000000110110000; mask_valid  = 1; end
+               4'd10 : begin mask_value   = 16'b0000001101100000; mask_valid  = 1; end
+               4'd11 : begin mask_value   = 16'b0000011101000000; mask_valid  = 1; end
+               4'd12 : begin mask_value   = 16'b0000110010100000; mask_valid  = 1; end
+               4'd13 : begin mask_value   = 16'b0001101100000000; mask_valid  = 1; end
+               4'd14 : begin mask_value   = 16'b0011010100000000; mask_valid  = 1; end
+               4'd15 : begin mask_value   = 16'b0111010000000000; mask_valid  = 1; end
+             default : begin mask_value   = 16'b0000000000000000; mask_valid  = 0; end
         endcase
     end // always
 
@@ -141,14 +109,14 @@ module lfsr_fibonacci
 (
     input  wire           clk,
     input  wire           rst_n,
-    input  wire  [4:0]    lfsr_length,
+    input  wire  [3:0]    lfsr_length,
     input  wire           lfsr_n_taps,
 
-    output reg  [31:0]    lfsr_value,
+    output reg  [15:0]    lfsr_value,
     output reg            lfsr_valid
 );
 
-    wire        [31:0]    mask_value_2_taps;
+    wire        [15:0]    mask_value_2_taps;
     wire                  mask_valid_2_taps;
 
     generate_mask_fibonacci_2_taps    gmf2t
@@ -158,7 +126,7 @@ module lfsr_fibonacci
         .mask_valid(mask_valid_2_taps)
     );
 
-    wire        [31:0]    mask_value_4_taps;
+    wire        [15:0]    mask_value_4_taps;
     wire                  mask_valid_4_taps;
 
     generate_mask_fibonacci_4_taps    gmf4t
@@ -168,7 +136,7 @@ module lfsr_fibonacci
         .mask_valid(mask_valid_4_taps)
     );
 
-    reg         [31:0]    mask_value;
+    reg         [15:0]    mask_value;
     reg                   mask_valid;
 
     always @(*)
@@ -192,19 +160,19 @@ module lfsr_fibonacci
         if      (~rst_n)
         begin
             // initialize current value/valid
-            lfsr_value       <= 32'd1;
+            lfsr_value       <= 16'd1;
             lfsr_valid       <= 1;
         end
         else if (~mask_valid)
         begin
             // force current value/valid to invalid
-            lfsr_value  <= 32'd0;
+            lfsr_value  <= 16'd0;
             lfsr_valid  <= 0;
         end
         else
         begin
             // shift the previous value and add in the computed (reduced) feedback value, set valid correctly (already verified mask is valid)
-            lfsr_value       <= { lfsr_value[30:0], ^(lfsr_value & mask_value) };
+            lfsr_value       <= { lfsr_value[14:0], ^(lfsr_value & mask_value) };
             lfsr_valid       <= 1;
         end
         // endif
@@ -250,7 +218,7 @@ module tt_um__kwr_lfsr__top // top-level (and business) logic
 
 // ////////////////////////////////////////////////////////////////////////
 
-    reg         [4:0]     length;
+    reg         [3:0]     length;
     reg                   n_taps;
     reg                   hold;
     reg                   step;
@@ -258,7 +226,7 @@ module tt_um__kwr_lfsr__top // top-level (and business) logic
     reg                   c_clk;
     reg                   c_clk_mask;
 
-    wire        [31:0]    value;
+    wire        [15:0]    value;
     wire                  valid;
 
     lfsr_fibonacci    lfsr
