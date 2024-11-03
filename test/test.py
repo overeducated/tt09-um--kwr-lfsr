8d1c571db2c19430f2ecd51e874bb2407ddd0f59
Kevin W. Rudd <ke6fzi@gmail.com>
2024-11-03 12:26:07 -0500

    another incremental tweak

--- test/test.py
+++ test/test.py
@@ -131,12 +131,12 @@ async def test_project(dut):
     assert dut.uio_out.value == 0xd9
     assert  dut.uo_out.value == 0xf1
 
-    dut.ui_in.value           = 0x05
+    dut.ui_in.value           = 0x85
     await  ClockCycles(dut.clk, 1)
     assert dut.uio_out.value == 0xd9
     assert  dut.uo_out.value == 0xf1
 
-    dut.ui_in.value           = 0x05
+    dut.ui_in.value           = 0x85
     await  ClockCycles(dut.clk, 1)
     assert dut.uio_out.value == 0xd9
     assert  dut.uo_out.value == 0xf1
